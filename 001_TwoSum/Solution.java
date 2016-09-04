e0be8848013cc3b09085ffa41f7a78c1a57e4b7c
林轩 <linxuan02@meituan.com>
2016-09-04 23:42:35 +0800

    add or change

---
+++ 001_TwoSum/Solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> m = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            m.put(nums[i], i + 1);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int sub = target - nums[i];
+            if(m.get(sub) == null) continue;
+            if(i + 1 == m.get(sub)) continue;
+            if(m.get(sub) != null){
+                result[0] = i + 1;
+                result[1] = m.get(sub);
+                break;
+            }
+        }
+        return result;
+    }
+
+}
